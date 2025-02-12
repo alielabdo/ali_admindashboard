d65e74271b65e1b48b70752cf34921e84fd41373
alielabdo <alloyali48@gmail.com>
2025-02-12 21:47:53 +0200

    app.js updated

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {GridComponent,ColumnsDirective,ColumnDirective,Page,Search,Edit,Inject,Toolbar} from '@syncfusion/ej2-react-grids'
+import {GridComponent,ColumnsDirective,ColumnDirective,Page,Search,Inject,Toolbar} from '@syncfusion/ej2-react-grids'
 
 import {employeesData,employeesGrid} from '../data/dummy'
 import { Header } from '../components'
